4b64e0af01ae3b25868f2314bf011bda019766ab
raberin <rmvaberin@gmail.com>
2019-08-28 19:54:32 -0700

    Added graphql/apollo boilerplate on index.js, added react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,10 +3,46 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App/App";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+// import { setContext } from "apollo-link-context";
+// import { AUTH_TOKEN } from "./constants";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//1
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// 2
+const httpLink = createHttpLink({
+  uri:
+    "mongodb+srv://foteekofficial:<password>@foteekcluster-nv3fj.mongodb.net/test?retryWrites=true&w=majority"
+});
+
+// 2.5
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem(AUTH_TOKEN);
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : ""
+//     }
+//   };
+// });
+
+// 3
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+// 4
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
