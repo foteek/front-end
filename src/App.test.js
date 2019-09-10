1cf35000d6b533042481b9da2c2d62c56fdcb1f4
sagdish <forsi1@yahoo.com>
2019-09-09 17:57:46 -0700

    add react-apollo testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { MockedProvider } from '@apollo/react-testing';
+
 import App from './App';
+import User from './components/User';
+
+afterEach(rtl.cleanup);
 
 describe('App', () => {
-  it ('it renders header with the text Learn React', () => {
+  it ('it renders renders without error', () => {
     const testDOM = rtl.render(<App />);
     // console.log('testDOM ', testDOM.debug());
-    const header = testDOM.queryByText(/learn react/i);
+    // const header = testDOM.queryByText(/learn react/i);
     // console.log(header.textContent);
-    expect(header).toBeInTheDocument();
+    
+    // <MockedProvider client={[]} >
+    //   <User />
+    // </MockedProvider>
+    
+    // expect(header).toBeInTheDocument();
   })
 });
